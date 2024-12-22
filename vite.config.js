8c36ba4697d154d430fb16f7c43c702694c06ce8
Shrey Singh <144680714+ShreySigh2912@users.noreply.github.com>
2024-12-22 13:18:03 +0530

    Update Vite config with correct base path

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Work-to-do/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: true
   }
 })
